ccd180e6bb174754d63f0833d5b476c02241517f
qq893821368 <893821368@qq.com>
2019-09-18 08:02:52 +0800

    new_classes_v0

---
+++ hbwm-bl/src/main/java/nchu/software/ruanko/hbwmbl/impl/MusicDisplayImpl.java
@@ -0,0 +1,5 @@
+package nchu.software.ruanko.hbwmbl.impl;
+
+public class MusicDisplayImpl {
+    //各类榜单和歌曲推荐
+}
---
+++ hbwm-util/src/main/java/nchu/software/ruanko/hbwmutil/util/ExceptionUtil.java
@@ -0,0 +1,8 @@
+package nchu.software.ruanko.hbwmutil.util;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionUtil {
+}
