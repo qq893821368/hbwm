5eea7185e9141b216ca093793e7f273db2d48f31
S16206121 <1764014198@qq.com>
2019-09-23 11:24:53 +0800

    resetPage

--- hbwm-ui/src/main/java/nchu/software/ruanko/hbwmweb/controller/testController.java
+++ hbwm-ui/src/main/java/nchu/software/ruanko/hbwmweb/controller/testController.java
@@ -1,12 +1,24 @@
 package nchu.software.ruanko.hbwmweb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class testController {
     @GetMapping("/")
     public String index(){
-        return "index";
+        return "login";
     }
+
+    @GetMapping("/login")
+    public String login(Model model){
+        return "login";
+    }
+
+    @GetMapping("/reset")
+    public String reset(){
+        return "reset";
+    }
+
 }
