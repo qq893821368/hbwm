d9aec1012d2d57b05436db7690fecf53dfeecfbb
S16206121 <1764014198@qq.com>
2019-09-23 09:15:26 +0800

    registerComplete

--- hbwm-common/src/main/java/nchu/software/ruanko/hbwmcommon/model/User.java
+++ hbwm-common/src/main/java/nchu/software/ruanko/hbwmcommon/model/User.java
@@ -1,4 +1,22 @@
 package nchu.software.ruanko.hbwmcommon.model;
 
 public class User {
+    private String name;
+    private String psw;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPsw() {
+        return psw;
+    }
+
+    public void setPsw(String psw) {
+        this.psw = psw;
+    }
 }
