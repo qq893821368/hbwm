c277acb7b5ff042b10204a47b00223f3e0002901
S16206122 <15207087271@163.com>
2019-09-25 11:43:00 +0800

    test-ljl1

--- hbwm-ui/src/main/java/nchu/software/ruanko/hbwmweb/controller/testController.java
+++ hbwm-ui/src/main/java/nchu/software/ruanko/hbwmweb/controller/testController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class testController {
     @GetMapping("/")
     public String index(){
-        return "index";
+        return "play";
     }
 }
